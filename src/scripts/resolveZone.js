ab8cbc71501b152ddde74afdec376115ac5bc0d7
NeftaliGC <aliguiacruz@outlook.com>
2023-09-29 19:19:40 -0600

    Ya funciona el teorema chino del residuo

--- src/scripts/resolveZone.js
+++ src/scripts/resolveZone.js
@@ -139,8 +139,8 @@ $rEqModulares.addEventListener('click', () => {
 
     // Crear un formulario dentro del div "resolveZone"
     $resolveZone.innerHTML = `
-        <h4>Aun no funciona</h4>
-        <form id="rEqModularesForm">
+        <h4>Creo que ya funciona</h4>
+        <form id="EqSistemForm">
             <input type="number" id="numero1" name="numero1" required>
             <label for="numero1">X</label>
             <label for="numero2">≡</label>
@@ -164,16 +164,19 @@ $rEqModulares.addEventListener('click', () => {
             <label for="numero3">(MOD </label>
             <input type="number" id="numero9" name="numero3" required>
             <label for="numero3"> )</label>
-            <button class="button" type="submit">Calcular</button>
+            <button class="button" id="submit">Calcular</button>
         </form>
-        <button class="button" type="add">Add</button>
+        <button class="button" id="add">Add</button>
+        <p>Por el momento solo se pueden de a 3</p>
     `;
 
-    /*
+    
     // Agregar un manejador de eventos para el botón "add"
-    const $add = $('add');
-    $add.addEventListener('click', () => {
-        $resolveZone.innerHTML += `
+    const add = document.getElementById('add');
+    const EqSistemForm = document.getElementById('EqSistemForm');
+    add.addEventListener('click', () => {
+        EqSistemForm.innerHTML += `
+            <br/>
             <input type="number" id="numero" name="numero1" required>
             <label for="numero1">X</label>
             <label for="numero2">≡</label>
@@ -182,22 +185,38 @@ $rEqModulares.addEventListener('click', () => {
             <input type="number" id="numero3" name="numero3" required>
             <label for="numero3"> )</label>
         `;
-    }); `*/
+    });
 
     // Agregar un manejador de eventos para el formulario
-    const rEqModularesForm = document.getElementById('rEqModularesForm');
-    rEqModularesForm.addEventListener('submit', (event) => {
+    //const rEqModularesForm = document.getElementById('rEqModularesForm');
+    EqSistemForm.addEventListener('submit', (event) => {
         event.preventDefault(); // Evitar que se recargue la página
         const numero1 = parseInt(document.getElementById('numero1').value);
         const numero2 = parseInt(document.getElementById('numero2').value);
         const numero3 = parseInt(document.getElementById('numero3').value);
+        const numero4 = parseInt(document.getElementById('numero4').value);
+        const numero5 = parseInt(document.getElementById('numero5').value);
+        const numero6 = parseInt(document.getElementById('numero6').value);
+        const numero7 = parseInt(document.getElementById('numero7').value);
+        const numero8 = parseInt(document.getElementById('numero8').value);
+        const numero9 = parseInt(document.getElementById('numero9').value);
 
-        if (Number.isInteger(numero1) && Number.isInteger(numero2) && Number.isInteger(numero3) && numero1 > 0 && numero2 > 0 && numero3 > 0) {
-            // Calcular el MCD y mostrar el resultado
-            let rEqModulares = calcREqModulares(numero1, numero2, numero3);
-            $resolveZone.innerHTML = `El rEqModulares de ${numero1}, ${numero2} y ${numero3} es ${rEqModulares}.`;
+        if (Number.isInteger(numero1) && Number.isInteger(numero2) && Number.isInteger(numero3) && numero1 > 0 && numero2 > 0 && numero3 > 0
+            && Number.isInteger(numero4) && Number.isInteger(numero5) && Number.isInteger(numero6) && numero4 > 0 && numero5 > 0 && numero6 > 0
+            && Number.isInteger(numero7) && Number.isInteger(numero8) && Number.isInteger(numero9) && numero7 > 0 && numero8 > 0 && numero9 > 0    
+        ) {
+            let coeficientes = [numero1, numero4, numero7];
+            console.log(coeficientes);
+            let numeros = [numero2, numero5, numero8];
+            console.log(numeros);
+            let modulos = [numero3, numero6, numero9];
+            console.log(modulos);
+
+            // Calcular el rEqModulares y mostrar el resultado
+            let rEqModulares = calcREqModulares(coeficientes, numeros, modulos);
+            //$resolveZone.innerHTML = `El rEqModulares de ${numero1}, ${numero2} y ${numero3} es ${3}.`;
         } else {
-            $resolveZone.innerHTML = 'Ingresa tres números enteros positivos válidos.';
+            $resolveZone.innerHTML = 'Ingresa números enteros positivos válidos.';
         }
     });
 });
@@ -215,7 +234,7 @@ function calcCombLineal(numero1, numero2) {
         const newX = y;
         const newY = x - Math.floor(numero1 / numero2) * y;
 
-        const result = [newX, newY, gcd];
+        const result = [newX, newY];
         return result;
     }
 }
@@ -277,4 +296,116 @@ function isPrimo(numero) {
     }
 
     return true;
+}
+
+function calcREqModulares(coeficientes, numeros, modulos) {
+    let cof = coeficientes;
+    let num = numeros;
+    let mod = modulos;
+
+    if (isPrimo(modulos[0]) && isPrimo(modulos[1]) && isPrimo(modulos[2])) {
+        //  Teorema chino del residuo
+        for(let i = 0; i < coeficientes.length; i++) {
+            if (coeficientes[i] != 1) {
+                for(let j = 1; j < modulos[i]; j++) {
+                    if ((coeficientes[i] * j) % modulos[i] == 1) {
+                        cof[i] = 1;
+                        num[i] = (numeros[i] * j) % modulos[i];
+                        mod[i] = modulos[i];
+                        break;
+                    } 
+                }
+            } else {
+                cof[i] = 1;
+                num[i] = numeros[i];
+                mod[i] = modulos[i];
+            }
+        }
+        console.log(cof);
+        for (let i = 0; i < cof.length; i++) {
+            $resolveZone.innerHTML += `${cof[i]}X ≡ ${num[i]} (MOD ${mod[i]})<br/>`;
+            
+        }
+
+        let n;
+        let q = [];
+        let r = [];
+        n = mod[0] * mod[1] * mod[2];
+        q[0] = n / mod[0];
+        q[1] = n / mod[1];
+        q[2] = n / mod[2];
+
+        for (let index = 0; index < q.length; index++) {
+            let temp = calcCombLineal(q[index], mod[index]);
+
+            let m1 = temp[0] % mod[index];
+            let m2 = temp[1] % mod[index];
+
+            if (m1 < 0) {
+                m1 += mod[index];
+            }
+            if (m2 < 0) {
+                m2 += mod[index];
+            }
+
+            if((m1 * q[index]) % mod[index] == 1) {
+                r[index] = m1;
+            } else {
+                r[index] = m2;
+            }
+            
+            /* este for no sirve, por que?, no se, pero no sirve 
+            Este for aunque su estructura es correcta, pero se buguea al no encontrar el valor de m
+            en la primera iteracion no hace la segunda iteracion y si intento corregir manualmente la variable de iteracion z
+            entra en un bucle infinito 
+
+            for (let z = 0; z < 2; z++) {
+                console.log(`inicial ${z}`);
+                if (temp[z] < mod[z]) {
+                    let m = temp[z] % mod[z];
+                    console.log(`m = ${temp[z]} % ${mod[z]} = ${m}`);
+                    if (m < 0) {
+                        m += mod[index];
+                    }
+                    if((m * q[index]) % mod[index] == 1) {
+                        console.log(`estado definitivo m = ${m}`);
+                        r[index] = m;
+                        break;
+                    }
+                    console.log(`estado transitivo m = ${m}`);
+                    console.log(`iteracion ${z}`);
+                }
+            } */
+        }
+        $resolveZone.innerHTML += `N = ${n}<br/> Q1 = ${q[0]}, Q2 = ${q[1]}, Q3 = ${q[2]}<br/> r1 = ${r[0]}, r2 = ${r[1]}, r3 = ${r[2]}<br/>`;
+
+        let x = num[0] * q[0] * r[0] + num[1] * q[1] * r[1] + num[2] * q[2] * r[2];
+        let resto = x % n;
+
+        $resolveZone.innerHTML += `X ≡ ${resto}(MOD ${n})<br/> Solucion general: X = K·${n} + ${resto}<br/>`;
+
+
+    } else {
+        for(let i = 0; i < coeficientes.length; i++) {
+            if (coeficientes[i] != 1) {
+                for(let j = 1; j < modulos[i]; j++) {
+                    if ((coeficientes[i] * j) % modulos[i] == 1) {
+                        cof[i] = 1;
+                        num[i] = (numeros[i] * j) % modulos[i];
+                        mod[i] = modulos[i];
+                        break;
+                    } 
+                }
+            } else {
+                cof[i] = 1;
+                num[i] = numeros[i];
+                mod[i] = modulos[i];
+            }
+        }
+        console.log(cof);
+        for (let i = 0; i < cof.length; i++) {
+            $resolveZone.innerHTML += `${cof[i]}X ≡ ${num[i]} (MOD ${mod[i]})<br/>`;
+            
+        }
+    }
 }
